aaabaa074669310e4fd54e58eee6c0d97c662830
ALiang02 <1745346447@qq.com>
2021-04-10 14:58:24 +0800

    拓扑图雏形

--- system-web/src/views/Content.vue
+++ system-web/src/views/Content.vue
@@ -53,6 +53,190 @@ export default {
     }
 
     node_map.stroke();
+    this.getTestData();
+    this.nodeDraw();
+  },
+
+  methods: {
+    getTestData() {
+      this.nodes = [
+        {
+          id: 1,
+          postion_x: 1,
+          postion_y: 1,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [3, 4],
+          color: "#000000",
+        },
+
+        {
+          id: 2,
+          postion_x: 2,
+          postion_y: 8,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [1],
+          color: "#27a7ce",
+        },
+
+        {
+          id: 3,
+          postion_x: 7,
+          postion_y: 3,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [1, 6],
+          color: "#7df782",
+        },
+
+        {
+          id: 4,
+          postion_x: 8,
+          postion_y: 5,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [2],
+          color: "#409EFF",
+        },
+
+        {
+          id: 5,
+          postion_x: 13,
+          postion_y: 1,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [3],
+          color: "#f77dd3",
+        },
+
+        {
+          id: 6,
+          postion_x: 4,
+          postion_y: 8,
+          environment_data: {
+            temperature: 22,
+            humidity: 30,
+          },
+          neighborhood_nodes: [4],
+          color: "#ff3d46",
+        },
+      ];
+    },
+    drawArrow(ctx, fromX, fromY, toX, toY, color, theta, headlen, width) {
+      //       ctx：Canvas绘图环境
+      // fromX, fromY：起点坐标（也可以换成p1，只不过它是一个数组）
+      // toX, toY：终点坐标 (也可以换成p2，只不过它是一个数组)
+      // theta：三角斜边一直线夹角
+      // headlen：三角斜边长度
+      // width：箭头线宽度
+      // color：箭头颜色
+      theta = 30;
+      headlen = 20;
+      width = 1;
+
+      // 计算各角度和对应的P2,P3坐标
+      var angle = (Math.atan2(fromY - toY, fromX - toX) * 180) / Math.PI,
+        angle1 = ((angle + theta) * Math.PI) / 180,
+        angle2 = ((angle - theta) * Math.PI) / 180,
+        topX = headlen * Math.cos(angle1),
+        topY = headlen * Math.sin(angle1),
+        botX = headlen * Math.cos(angle2),
+        botY = headlen * Math.sin(angle2);
+
+      ctx.save();
+      ctx.beginPath();
+
+      var arrowX = fromX - topX,
+        arrowY = fromY - topY;
+      var x = (fromX - toX) / 2;
+      var y = (fromY - toY) / 2;
+      // ctx.setLineDash([10, 10]); // [实线长度, 间隙长度]
+      ctx.moveTo(arrowX, arrowY);
+      ctx.moveTo(fromX, fromY);
+      ctx.lineTo(toX, toY);
+      // ctx.stroke();
+      arrowX = toX + topX;
+      arrowY = toY + topY;
+      // ctx.setLineDash([1, 0]); // [实线长度, 间隙长度]
+
+      ctx.moveTo(arrowX + x, arrowY + y);
+      ctx.lineTo(toX + x, toY + y);
+      arrowX = toX + botX;
+      arrowY = toY + botY;
+      ctx.lineTo(arrowX + x, arrowY + y);
+      ctx.strokeStyle = color;
+
+      ctx.lineWidth = width;
+      ctx.stroke();
+      ctx.restore();
+    },
+    nodeDraw() {
+      var map = document.getElementById("map");
+      var node_map = map.getContext("2d");
+
+      for (let node of this.nodes) {
+        node_map.setLineDash([1, 0]); // [实线长度, 间隙长度]
+        node_map.beginPath();
+        node_map.arc(
+          node.postion_x * 100,
+          (9 - node.postion_y) * 100,
+          30,
+          0,
+          Math.PI * 2,
+          false
+        );
+
+        // node_map.fillStyle = `rgba(${parseInt(
+        //   node.color.substring(1, 3),
+        //   16
+        // )},${parseInt(node.color.substring(1, 3), 16)},${parseInt(
+        //   node.color.substring(1, 3),
+        //   16
+        // )},1)`;
+        node_map.strokeStyle = node.color;
+        node_map.stroke();
+        node_map.fillText(
+          node.environment_data.temperature +
+            "℃" +
+            " " +
+            node.environment_data.humidity +
+            "%",
+          node.postion_x * 100 - 15,
+          (9 - node.postion_y) * 100
+        );
+        for (let node_id of node.neighborhood_nodes) {
+          for (let { id, postion_x, postion_y, color } of this.nodes) {
+            if (node_id == id) {
+              this.drawArrow(
+                node_map,
+
+                postion_x * 100,
+                (9 - postion_y) * 100,
+                node.postion_x * 100,
+                (9 - node.postion_y) * 100,
+                color
+              );
+            }
+          }
+        }
+      }
+      // this.drawArrow(node_map, 200, 300, 400, 500);
+    },
+
+    randomInt(from, to) {
+      return parseInt(Math.random() * (to - from + 1) + from);
+    },
   },
 };
 </script>
